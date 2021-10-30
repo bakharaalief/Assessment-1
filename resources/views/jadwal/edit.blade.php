be0003fe7a9456ba91f9ddd27094dc0b84d8fdc9
bakharaalief <50726376+bakharaalief@users.noreply.github.com>
2021-10-30 22:59:10 +0700

    update view edit in jadwal

--- resources/views/jadwal/edit.blade.php
+++ resources/views/jadwal/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Create Jadwal</div>
+                <div class="card-header">Update Jadwal</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('jadwal.update', ['jadwal' => $jadwal->id]) }}">
@@ -129,7 +129,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Create
+                                    Update
                                 </button>
                             </div>
                         </div>
